475e25811841db69d4b89b33d573cb6e797a04a2
Jan Groth <jan.groth.de@gmail.com>
2017-12-31 12:22:09 +1100

    change lambda to write last-update file

--- lambda/pushtobucket.py
+++ lambda/pushtobucket.py
@@ -1,30 +1,32 @@
 import boto3
-import datetime
-import random
 import os
 import logging
+from datetime import datetime
+from dateutil import tz
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-file_name = 'test.txt'
+file_name = 'last-update'
 
 def add_tmp_path(file_name):
     return os.path.join('/tmp', file_name)
 
-def create_random_file(file_name):
-    content = random.randint(1, 1000) 
+def create_timestamp_file(file_name):
+    utc = datetime.utcnow()
+    to_zone = tz.gettz('Australia/Sydney')
+    syd = datetime.utcnow().replace(tzinfo=to_zone).astimezone(to_zone)
     with open(add_tmp_path(file_name),'w+') as f:
-        f.write('{} - {}\n'.format(datetime.datetime.now(), content))
+        f.write('{} (UTC)\n{} (SYD)\n'.format(utc, syd))
         f.close()
 
 def copy_file_to_bucket(file_name, bucket_name):
     s3 = boto3.client('s3')
     with open(add_tmp_path(file_name),'rb') as f:
         response = s3.put_object(
-            Key=file_name, 
-            Body=f, 
-            Bucket=bucket_name, 
+            Key=file_name,
+            Body=f,
+            Bucket=bucket_name,
             StorageClass='REDUCED_REDUNDANCY',
             ContentType='text/plain'
         )
@@ -33,8 +35,8 @@ def copy_file_to_bucket(file_name, bucket_name):
 
 def handler(event, context):
     logger.info('Invoking handler')
-    bucket_name = os.environ['WEBSITE_BUCKET']
-    create_random_file(file_name)
+    bucket_name = os.environ.get('WEBSITE_BUCKET', 'local')
+    create_timestamp_file(file_name)
     copy_file_to_bucket(file_name, bucket_name)
     logger.info('Finished successfully.')
 
--- scripts/upload-lambda.sh
+++ scripts/upload-lambda.sh
@@ -6,3 +6,4 @@ cd ../lambda
 zip pushtobucket.zip pushtobucket.py
 
 aws s3 cp pushtobucket.zip s3://${CODE_BUCKET_NAME}/ --storage-class REDUCED_REDUNDANCY
+aws s3api list-object-versions --bucket ${CODE_BUCKET_NAME} --max-items 1 | grep 'VersionId'
