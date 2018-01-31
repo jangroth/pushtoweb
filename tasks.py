4e9dc0b152d77f232a1f1465fab5f5ba6849023f
Jan Groth <jan.groth.de@gmail.com>
2018-02-01 08:13:24 +1100

    Add update task

--- tasks.py
+++ tasks.py
@@ -2,5 +2,12 @@ from invoke import task
 
 
 @task
-def hello(ctx):
-    print('hello world')
+def update(ctx, what):
+    """
+    Update existing stack (web|code)
+    """
+    whats = ['web', 'code']
+    if what not in whats:
+        print('Allowed arguments: {}'.format(whats))
+    else:
+        ctx.run('bash scripts/update-{}-stack.sh')
