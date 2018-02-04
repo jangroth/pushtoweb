b6a8d2ecd5bc2485eb855842afab9dccd20a63c7
Jan Groth <jan.groth.de@gmail.com>
2018-02-05 07:52:04 +1100

    Clean up lambda

--- lambda/cloneintobucket.py
+++ lambda/cloneintobucket.py
@@ -15,11 +15,11 @@ logger.setLevel(logging.INFO)
 
 
 class RepoToBucket:
-    def __init__(self, repo_url, bucket_name, local_docker_run):
+    def __init__(self, repo_url, bucket_name, runs_local):
         self.repo_url = repo_url
         self.bucket_name = bucket_name
-        self.local_docker_run = local_docker_run
-        self.temp_dir = tempfile.mkdtemp(prefix='lambda_')
+        self.runs_local = runs_local
+        self.temp_dir = tempfile.mkdtemp(prefix='RepoToBucket_')
         self.s3 = boto3.client('s3')
 
     def _get_path_to(self, *file_name):
@@ -29,7 +29,7 @@ class RepoToBucket:
         tempdir_end = [m.start() for m in re.finditer(r"/", full_path)][2]
         return full_path[tempdir_end + 1:]
 
-    def configure_git(self):
+    def _configure_git(self):
         os.system('tar -C {} -xf git-2.4.3.tar'.format(self.temp_dir))
         os.environ['GIT_EXEC_PATH'] = self._get_path_to('usr/libexec/git-core')
         os.environ['GIT_TEMPLATE_DIR'] = self._get_path_to('/usr/share/git-core/templates')
@@ -40,8 +40,6 @@ class RepoToBucket:
             self._get_path_to('/usr/share/git-core/templates'), self._get_path_to('usr/libexec/git-core'),
             self.repo_url, repo_path))
         shutil.rmtree(self._get_path_to(repo_path, '.git'), ignore_errors=True)
-        if self.local_docker_run:
-            time.sleep(100)
 
     def _create_timestamp(self):
         utc = datetime.utcnow()
@@ -51,6 +49,15 @@ class RepoToBucket:
             f.write('{} (UTC)\n{} (SYD)\n'.format(utc, syd))
             f.close()
 
+    def _copy_into_bucket(self, bucket_key, f):
+        self.s3.put_object(
+            Key=bucket_key,
+            Body=f,
+            Bucket=self.bucket_name,
+            StorageClass='REDUCED_REDUNDANCY',
+            ContentType='text/plain'
+        )
+
     def _copy_files_into_bucket(self):
         for subdir, dirs, files in os.walk(self.temp_dir):
             for file in [x for x in files if not x.startswith('.')]:
@@ -58,19 +65,13 @@ class RepoToBucket:
                 bucket_key = self._strip_temp_dir(full_path)
                 logger.info("Uploading {} as {}...".format(full_path, bucket_key))
                 with open(full_path, 'rb') as f:
-                    response = self.s3.put_object(
-                        Key=bucket_key,
-                        Body=f,
-                        Bucket=self.bucket_name,
-                        StorageClass='REDUCED_REDUNDANCY',
-                        ContentType='text/plain'
-                    )
+                    self._copy_into_bucket(bucket_key, f)
 
     def copy_repo_into_bucket(self):
-        self.configure_git()
+        self._configure_git()
         self._clone_repo()
-        # self._create_timestamp()
-        # self._copy_files_into_bucket()
+        self._create_timestamp()
+        self._copy_files_into_bucket()
 
 
 def handler(event, context):
@@ -78,9 +79,9 @@ def handler(event, context):
 
     repo_url = os.environ.get('REPO_URL', 'https://jangroth@bitbucket.org/jangroth/test-repo.git')
     bucket_name = os.environ.get('WEBSITE_BUCKET', 'ptw-web-websitebucket-aa10mrx6putg')
-    local_docker_run = event.get('local', None)
+    runs_local = event.get('local', None)
 
-    RepoToBucket(repo_url, bucket_name, local_docker_run).copy_repo_into_bucket()
+    RepoToBucket(repo_url, bucket_name, runs_local).copy_repo_into_bucket()
 
     logger.info('Finished successfully.')
 
