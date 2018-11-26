8d7889ea06ae72e8dfeaffcb2987b6444743a53d
Jan Groth <jan.groth.de@gmail.com>
2018-11-26 22:23:47 +1100

    Package lambda as non-root user

--- scripts/package-lambda-with-container.sh
+++ scripts/package-lambda-with-container.sh
@@ -6,4 +6,5 @@ docker run --rm \
     --volume ${parent_folder}/scripts:/scripts:ro \
     --volume ${parent_folder}/code:/code:ro \
     --volume ${parent_folder}/build:/build \
+    --user 1000:1000 \
     ptw:lambdapackage /scripts/package.sh
